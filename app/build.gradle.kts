a8e1308a22cdb269a5eb3d6630106dfcafe8a7a5
Sahil-Mandal15 <sahilmandal06@gmail.com>
2025-01-24 01:59:39 +0530

    added firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -15,6 +18,15 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val keyStoreFile = project.rootProject.file("secretkeys.properties")
+        val properties = Properties()
+        properties.load(keyStoreFile.inputStream())
+
+        val googleClientId = properties.get("GOOGLE_CLIENT_ID") ?: ""
+
+        buildConfigField("String", "GOOGLE_CLIENT_ID", googleClientId.toString())
+
     }
 
     buildTypes {
@@ -33,6 +45,11 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+        dataBinding = true
+        buildConfig = true
+    }
 }
 
 dependencies {
@@ -45,4 +62,16 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation ("androidx.credentials:credentials:1.5.0-rc01")
+    implementation ("androidx.credentials:credentials-play-services-auth:1.5.0-rc01")
+    implementation ("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+
+    // Firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.8.0"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-firestore")
+
+
+
 }
---
+++ app/src/main/java/com/sahilm/medmate/auth/AuthClient.kt
@@ -0,0 +1,96 @@
+package com.sahilm.medmate.auth
+
+import android.content.Context
+import androidx.credentials.ClearCredentialStateRequest
+import androidx.credentials.CredentialManager
+import androidx.credentials.CustomCredential
+import androidx.credentials.GetCredentialRequest
+import androidx.credentials.GetCredentialResponse
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption
+import com.google.android.libraries.identity.googleid.GoogleIdTokenCredential
+import com.google.android.libraries.identity.googleid.GoogleIdTokenParsingException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.sahilm.medmate.BuildConfig
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.tasks.await
+
+class AuthClient(
+    private val context: Context
+) {
+
+    private var credentialManager: CredentialManager? = null
+    private var firebaseAuth = FirebaseAuth.getInstance()
+    private val googleClientId = BuildConfig.GOOGLE_CLIENT_ID
+
+    suspend fun signIn () : Boolean {
+        if (isSignedIn()) {
+            true
+        }
+        try {
+            val result = getCredentialRequest()
+            return handleSignIn(result)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            if (e is CancellationException) throw e
+
+            return false
+        }
+    }
+
+    private suspend fun getCredentialRequest(): GetCredentialResponse {
+        val request = GetCredentialRequest.Builder()
+            .addCredentialOption(
+                GetGoogleIdOption.Builder()
+                    .setFilterByAuthorizedAccounts(false)
+                    .setServerClientId(googleClientId)
+                    .setAutoSelectEnabled(false)
+                    .build()
+            ).build()
+
+        return credentialManager!!.getCredential(context, request)
+    }
+
+    fun isSignedIn(): Boolean {
+        if (firebaseAuth.currentUser != null) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    suspend fun signOut() {
+        credentialManager!!.clearCredentialState(
+            ClearCredentialStateRequest()
+        )
+        firebaseAuth.signOut()
+    }
+
+    fun clearCredentialManager() {
+        credentialManager = null
+    }
+
+    private suspend fun handleSignIn(result: GetCredentialResponse): Boolean {
+        val credential = result.credential
+
+        if (
+            credential is CustomCredential &&
+            credential.type == GoogleIdTokenCredential.TYPE_GOOGLE_ID_TOKEN_CREDENTIAL
+        ) {
+            try {
+                val tokenCredential = GoogleIdTokenCredential.createFrom(credential.data)
+
+                val authCredential = GoogleAuthProvider.getCredential(
+                    tokenCredential.idToken, null
+                )
+                val authResult = firebaseAuth.signInWithCredential(authCredential).await()
+
+                return authResult.user != null
+            } catch (e: GoogleIdTokenParsingException) {
+                return false
+            }
+        } else {
+            return false
+        }
+    }
+}
